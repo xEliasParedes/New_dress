69c91b25760c1f514712bd14964e3aa51587f810
xEliasParedes <Paredeselias7@gmail.com>
2024-08-31 18:48:36 -0400

    Agrega - 5 paginas para la app

---
+++ src/app/pages/exploracion/exploracion.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exploracion',
+  templateUrl: './exploracion.page.html',
+  styleUrls: ['./exploracion.page.scss'],
+})
+export class ExploracionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
