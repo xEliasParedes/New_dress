4ac5cb2daafa71dde68175cf83bda2781764fd2b
xEliasParedes <Paredeselias7@gmail.com>
2024-08-31 18:33:14 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NEW_DRESS',
+  webDir: 'www'
+};
+
+export default config;
