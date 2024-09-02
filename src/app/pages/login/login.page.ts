7701f1eeceb7f52a5b5781ee238815bf0c0f890b
xEliasParedes <Paredeselias7@gmail.com>
2024-09-01 22:18:38 -0400

    agrega - funciones de validacion e incorpora alert

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,53 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(public router:Router) { }
+  alertButtons = ['ingresar()']
+
+  constructor(public router:Router,private alertController:AlertController) { }
 
   ngOnInit() {
   }
 
-  ingresar(){
-    this.router.navigate(['/home'])
+  field:string=""
+  validarCampo(model:any){
+    for(var [key,value] of Object.entries(model)){
+      if(value == ""){
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
   }
+
+  async presentAlert(titulo:string,sub_titulo:string,mensaje:string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      subHeader: sub_titulo,
+      message: mensaje,
+      buttons: ['Action'],
+    });
+
+    await alert.present();
+  }
+
+
+
+  login:any={
+    usuario:"",
+    password:""
+  }
+  ingresar(){
+    if(this.validarCampo(this.login)){
+      let NavigationExtras:NavigationExtras={
+        state:{login: this.login}
+      };
+      this.router.navigate(['/home'],NavigationExtras);
+    }else{
+      this.presentAlert("Columnas Vacias", "no puedes iniciar sesion", "el siguiente campo: " + this.field + " esta vacio")
+    }
+    
+  }
+
+  
 
 }
