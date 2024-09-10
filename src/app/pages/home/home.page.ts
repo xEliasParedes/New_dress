640aa7500afc0f71eb14231dc09a33c418126311
xEliasParedes <Paredeselias7@gmail.com>
2024-09-10 00:04:10 -0300

    COMENTARIOS D ECODIGO

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,10 +14,13 @@ export class HomePage implements OnInit {
   usuario: string ='';
 
   constructor(private router:Router) {
-
+      //la constante crea un objeto receptor
       const navigation = this.router.getCurrentNavigation();
+      //si en el objeto hay datos
       if(navigation?.extras.state){
+        //guarda en state los datos recibidos
         const state = navigation.extras.state as {login:{ usuario: string; password: string}};
+        //guarda en usuario, los datos de state
         this.usuario = state.login.usuario;
       }
   }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -8,32 +8,37 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+//modelo para obtener los datos de inicio
   login:any={
     usuario:"",
     password:""
 }
-
+  //activa la alerta cuando la funcion se ejecuta
   alertButtons = ['ingresar()']
 
-  constructor(public router:Router,private alertController:AlertController) { }
+  constructor(public router:Router ,private alertController:AlertController) { }
 
   ngOnInit() {
   }
-
+//valida el modelo y sus atributos, si es true pasa los datos al state
   validateLogin(){
     if(this.validarCampo(this.login)){
       let NavigationExtras : NavigationExtras ={
         state:{login : this.login}
       };
+      //lleva la pagina a home junto a los datos del state(NavigationExtras)
       this.router.navigate(['/home'], NavigationExtras);
     }
   }
-
+  //una variable que representa el campo vacio
   field:string=""
+  //funcion que recibe un modelo cualquiera(login, registro, etc)
   validarCampo(model:any){
+    //recorre cada campo y valor del modelo/objeto
     for(var [key,value] of Object.entries(model)){
+      //muestra por consola el campo, nombre del campo
       console.log(key)
+      //determina si esta vacio value(valor del campo), si es true, guarda en field el nombre del campo
       if(value == ""){
         this.field = key;
         console.log(value)
@@ -42,7 +47,7 @@ export class LoginPage implements OnInit {
     }
     return true;
   }
-
+  //estructura de la alerta
   async presentAlert(titulo:string,sub_titulo:string,mensaje:string) {
     const alert = await this.alertController.create({
       header: titulo,
@@ -50,7 +55,7 @@ export class LoginPage implements OnInit {
       message: mensaje,
       buttons: ['Action'],
     });
-
+    //este genera la alerta y la pone en espera(para cuando se active)
     await alert.present();
   }
 
@@ -58,6 +63,7 @@ export class LoginPage implements OnInit {
 
 
   ingresar(){
+    //usa la funcion validar campo con el modelo login, establecido anteriormente
     if(this.validarCampo(this.login)){
       let NavigationExtras:NavigationExtras={
         state:{login: this.login}
@@ -65,10 +71,12 @@ export class LoginPage implements OnInit {
       console.log(this.login)
       this.router.navigate(['/home'],NavigationExtras);
     }else{
+      //si los campos son vacios, te manda la siguiente alerta
       this.presentAlert("Columnas Vacias", "no puedes iniciar sesion", "el siguiente campo: " + this.field + " esta vacio")
     }
     
   }
+  
 
   
 
