47aa3996f0553ebc12bfcc5e58d24d0bbc8575f1
xEliasParedes <Paredeselias7@gmail.com>
2024-09-09 21:11:34 -0300

    incorporacion de funcionalidades- restablecer

--- src/app/pages/restablecer/restablecer.page.ts
+++ src/app/pages/restablecer/restablecer.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restablecer',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restablecer.page.scss'],
 })
 export class RestablecerPage implements OnInit {
+  restablecer:any={
+    Email:"",
+    
+  }
 
-  constructor() { }
+  alertButtons = ['buscar()']
+
+  constructor(public router:Router,private alertController:AlertController) { }
 
   ngOnInit() {
+  }
+
+  field:string=""
+  validarCampo(model:any){
+    for(var [key,value] of Object.entries(model)){
+      console.log(key)
+      if(value == ""){
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  async presentAlert(titulo:string,sub_titulo:string,mensaje:string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      subHeader: sub_titulo,
+      message: mensaje,
+      buttons: ['Action'],
+    });
+
+    await alert.present();
+  }
+
+
+
+
+  buscar(){
+    if(this.validarCampo(this.restablecer)){
+      let NavigationExtras:NavigationExtras={
+        state:{restablecer: this.restablecer}
+      };
+      this.router.navigate(['/login'],NavigationExtras);
+    }else{
+      this.presentAlert("Error- Columna Vacia", "no puedes buscar un usuario", "el siguiente campo: " + this.field + " esta vacio")
+    }
+    
   }
 
 }
