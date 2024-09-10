14619ecc2a7ccdc495a1c00354c6f6e09356d37d
cuchilloxde <101745496+cuchilloxde@users.noreply.github.com>
2024-09-09 22:55:11 -0300

    Home page, intentando que reconozca al usuario.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+  usuario: string ='';
+
+  constructor(private router:Router) {
+
+      const navigation = this.router.getCurrentNavigation();
+      if(navigation?.extras.state){
+        const state = navigation.extras.state as {login:{ usuario: string; password: string}};
+        this.usuario = state.login.usuario;
+      }
+  }
+
+  ngOnInit() {}
 
 }
