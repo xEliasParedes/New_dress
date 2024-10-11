f8eaae912b5781f4b070be56679022c75cc584fe
xEliasParedes <Paredeselias7@gmail.com>
2024-10-11 00:25:46 -0300

    añade - barra de busqueda con posibles resultados y filtrado

--- src/app/pages/exploracion/exploracion.page.ts
+++ src/app/pages/exploracion/exploracion.page.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exploracion.page.scss'],
 })
 export class ExploracionPage implements OnInit {
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ]; //se debe remplazara por la data de los titulos cuando se incorpore firebase;
+  public results = [...this.data];
+
+
+
+  
+  handleInput(event:any) {
+    const query = event.target.value.toLowerCase();
+    this.results = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1) ;
+  }
+
+  
 
   constructor() { }
 
