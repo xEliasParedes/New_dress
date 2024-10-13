b7bee00d4fa6cde27a046a2399fa02d6af5fca33
xEliasParedes <Paredeselias7@gmail.com>
2024-10-12 23:11:15 -0300

    modifica - incorpora route guard y comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BarraComponent } from './components/barra/barra.component';
 
 @NgModule({
-  declarations: [AppComponent, BarraComponent],
+  declarations: [AppComponent, BarraComponent],//se añade el componente para ser usado en routing module
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
