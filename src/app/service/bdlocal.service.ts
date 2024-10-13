044c24dc1b89f16f2261be79a76098e58bda4c79
Cuchilloxde <kathleendanielajiji@gmail.com>
2024-10-13 16:25:53 -0300

    Persistencia LocalStorage - 0.2

--- src/app/service/bdlocal.service.ts
+++ src/app/service/bdlocal.service.ts
@@ -12,11 +12,18 @@ export class BdlocalService {
   private _storage: Storage | null = null;
   constructor(private storage: Storage, public toastController: ToastController) {
     this.Init();
-    this.guardarUsuario();
+    this.cargarUsuario();
    }
    async Init() {
     const storage = await this.storage.create();
     this._storage = storage;
+   }
+
+   async cargarUsuario(){
+    const miSesion = await this.storage.get('sesion');
+    if(miSesion){
+      this.sesion = miSesion;
+    }
    }
 
    guardarUsuario(nombre: string, contrasena: string){
@@ -24,6 +31,43 @@ export class BdlocalService {
     if(!existe){
       this.sesion.unshift({strNombre: nombre, strContrasena: contrasena}) //
       this._storage?.set('sesion', this.sesion);
+      this.presentToast('Usuario guardado')
+    }else{
+      this.presentToast('El usuario ya existe')
     }
    }
+
+   eliminarUsuario(nombre: string){
+    const existe = this.sesion.find(c => c.strNombre === nombre );
+    if(!existe){
+      this.sesion = this.sesion.filter(c => c.strNombre !== nombre);
+      this._storage?.set('sesion', this.sesion);
+      this.presentToast('Usuario eliminado')
+    }else{
+      this.presentToast('El usuario ya existe')
+    }
+   }
+
+   borrarBD(){
+    this._storage?.clear();
+    this.sesion = [];
+    console.log(this.sesion.length);
+   }
+
+   async presentToast(mensaje:string){
+    const toast = await this.toastController.create({
+      message: mensaje,
+      translucent: true,
+      color: 'medium',
+      position: 'top',
+      duration: 3000
+    })
+    toast.present();
+   }
+
+   async mostrarBD(){
+    return this.sesion;
+   }
+    
+   
 }
