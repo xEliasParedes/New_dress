7666e6776f0bb8d0aa97a3d150a214dc3e8d7c3c
xEliasParedes <Paredeselias7@gmail.com>
2024-10-14 01:17:44 -0300

    parche - reintegracion de ionic storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
-
-
-
-
 import { BarraComponent } from './components/barra/barra.component';
 
 @NgModule({
   declarations: [AppComponent, BarraComponent],//se añade el componente para ser usado en routing module
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
 
 
 
