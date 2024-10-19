45d80efaff129bb7fde029532de3e4022af43def
xEliasParedes <Paredeselias7@gmail.com>
2024-10-18 21:52:20 -0300

    modifica - estructura, funciones y estilos de home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 import { BdlocalService } from 'src/app/services/bdlocal.service';
 
 
@@ -18,7 +19,13 @@ export class HomePage implements OnInit {
   
   usuarios: any = []; //Atraemos los ususarios desde la Base de Datos.
 
-  constructor(private router:Router, private bdLocalService: BdlocalService) {
+  apiData: any;
+  temperature: number | undefined;
+  weatherState: string | undefined;
+  weatherIconUrl: string | undefined;
+
+
+  constructor(private router:Router, private bdLocalService: BdlocalService, private apiService:ApiService) {
       //la constante crea un objeto receptor
       const navigation = this.router.getCurrentNavigation();
       //si en el objeto hay datos
@@ -48,7 +55,35 @@ export class HomePage implements OnInit {
     this.usuarios = this.bdLocalService.mostrarBD();
   }
 
-  ngOnInit() {}
+ 
+
+  
+  
+  ngOnInit() {
+    //cordenadas del duoc/cercania duoc
+    const lat = -33.033563887809265; 
+    const lon = -71.53308076902319;
+    
+    this.apiService.getWeather(lat, lon).subscribe({
+      next: (data) => {
+        this.apiData = data;
+        // Obtener la temperatura y el estado del clima
+        this.temperature = data.main.temp;
+        this.weatherState = data.weather[0].description;
+        
+        // Crear la URL del icono basado en el ID proporcionado
+        const iconCode = data.weather[0].icon;
+        this.weatherIconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      },
+      error: (error) => {
+        console.error('Error al obtener los datos del clima:', error);
+      },
+      complete: () => {
+        console.log('Solicitud completada');
+      }
+    });
+    
+  }
 
 
 }
